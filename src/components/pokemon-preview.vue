c99d7cfcc589a0d29aed40478095f5f2c459034b
StrukovSergei <strukov.sergei.999@gmail.com>
2023-08-15 11:48:15 +0300

    first try at hp bar

--- src/components/pokemon-preview.vue
+++ src/components/pokemon-preview.vue
@@ -3,6 +3,10 @@
     <div class="poke-sprite"><img :src="getImg" alt=""></div>
     <div class="poke-name">{{ pokemon.name }}</div>
     <div class="poke-class">{{ getTypes }}</div>
+    <div class="poke-hp">
+      <el-progress :text-inside="true" :stroke-width="getHpBarWidth" :percentage="getHpPercentage"
+        :status="getHpStatus" />
+    </div>
   </section>
 </template>
 
@@ -18,9 +22,30 @@ export default {
     },
     getTypes() {
       return this.pokemon.type.join(', ')
-    }
+    },
+    getHpPercentage() {
+      return (this.pokemon.hp / this.pokemon.maxHp) * 100
+    },
+    getHpBarWidth() {
+      // You can adjust this value to control the thickness of the progress bar
+      return 20
+    },
+    getHpStatus() {
+      if (this.getHpPercentage > 75) {
+        return 'success'
+      } else if (this.getHpPercentage > 50) {
+        return 'warning'
+      } else {
+        return 'exception'
+      }
+    },
   }
 }
 </script>
 
-<style></style>
+<style>
+.demo-progress .el-progress--line {
+  margin-bottom: 15px;
+  width: 350px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { store } from './store'
 import router from './router'
 import { icon } from './directives'
+import ElementPlus from 'element-plus'
 
 import './assets/main.scss'
 
@@ -11,4 +12,5 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 app.directive('icon', icon)
+app.use(ElementPlus)
 app.mount('#app')
