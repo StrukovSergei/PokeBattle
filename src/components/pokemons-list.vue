f0bff524b67891e8f3262e8ca13f00147f174824
StrukovSergei <strukov.sergei.999@gmail.com>
2023-08-19 18:17:37 +0300

    first logic between attack method

--- src/components/pokemons-list.vue
+++ src/components/pokemons-list.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="pokemons-list flex text-center">
-    <pokemon-preview v-for="pokemon in pokemons" :key="pokemon._id" :pokemon="pokemon" />
+    <pokemon-preview v-for="pokemon in pokemons" :key="pokemon._id" :pokemon="pokemon" @attack="userAttack" />
   </section>
 </template>
 
@@ -11,6 +11,15 @@ export default {
   props: {
     pokemons: Array,
   },
+
+  methods: {
+    userAttack(attackData) {
+      // Handle the user's attack logic here
+      const { attacker, move } = attackData
+      // Calculate damage, update PC's Pokemon HP, and handle the PC's move
+    },
+  },
+
   components: {
     pokemonPreview,
   },
