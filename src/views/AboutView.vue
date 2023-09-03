376ca9ee8381f684797c2e6baf5481a9a4a9b062
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-03 08:06:42 +0300

    fixed svg bug

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1> <i v-html="$svg('fighting')"></i>This is un about page</h1>
+    <h1> <i v-html="$svg('fairy')"></i>This is un about page</h1>
   </div>
 </template>
