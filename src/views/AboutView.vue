3d6ded5bb3a6c038b43c6af0dc2d5333b85a51a3
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-07 22:45:57 +0300

    fixed couple svg's not showing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,9 +4,12 @@
     <h1> <i v-html="$svg('fairy')"></i>This is un about page</h1>
     <h1> <i v-html="$svg('psychic')"></i>This is un about page</h1>
     <h1> <i v-html="$svg('normal')"></i>This is un about page</h1>
+
     <h1> <i v-html="$svg('electric')"></i>This is un about page</h1>
     <h1> <i v-html="$svg('dragon')"></i>This is un about page</h1>
     <h1> <i v-html="$svg('ground')"></i>This is un about page</h1>
+    
+
     <h1> <i v-html="$svg('rock')"></i>This is un about page</h1>
   </div>
 </template>
