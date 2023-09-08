6a82d9fbb23ab45755aeeb0abc24d2a020caff45
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-08 18:03:30 +0300

    added svg class

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="about">
-    <h1> <i v-html="$svg('fighting')"></i>This is un about page</h1>
-    <h1> <i v-html="$svg('fairy')"></i>This is un about page</h1>
-    <h1> <i v-html="$svg('psychic')"></i>This is un about page</h1>
-    <h1> <i v-html="$svg('normal')"></i>This is un about page</h1>
+    <h1 class="type-svg" > <i v-html="$svg('fighting')"></i>This is un about page</h1>
+    <h1 class="type-svg" > <i v-html="$svg('fairy')"></i>This is un about page</h1>
+    <h1 class="type-svg" ><i  v-html="$svg('psychic')"></i>This is un about page</h1>
+    <h1 class="type-svg" ><i  v-html="$svg('normal')"></i>This is un about page</h1>
 
-    <h1> <i v-html="$svg('electric')"></i>This is un about page</h1>
-    <h1> <i v-html="$svg('dragon')"></i>This is un about page</h1>
-    <h1> <i v-html="$svg('ground')"></i>This is un about page</h1>
+    <h1 class="type-svg" ><i  v-html="$svg('electric')"></i>This is un about page</h1>
+    <h1 class="type-svg" ><i  v-html="$svg('dragon')"></i>This is un about page</h1>
+    <h1 class="type-svg" ><i  v-html="$svg('ground')"></i>This is un about page</h1>
     
 
-    <h1> <i v-html="$svg('rock')"></i>This is un about page</h1>
+    <h1 class="type-svg" ><i  v-html="$svg('rock')"></i>This is un about page</h1>
   </div>
 </template>
