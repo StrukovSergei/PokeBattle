dd6a61e9e8b6e31657deca723ce18b09c216a127
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-09 12:22:03 +0300

    made svg's look better

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1 class="type-svg" > <i v-html="$svg('fighting')"></i>This is un about page</h1>
+    <h1 class="type-svg" > <i class="fighting" v-html="$svg('fighting')"></i></h1>
     <h1 class="type-svg" > <i v-html="$svg('fairy')"></i>This is un about page</h1>
     <h1 class="type-svg" ><i  v-html="$svg('psychic')"></i>This is un about page</h1>
     <h1 class="type-svg" ><i  v-html="$svg('normal')"></i>This is un about page</h1>
