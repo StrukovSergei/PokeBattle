19b052ef0a7b6f522feff08909af7cf610d5a57d
StrukovSergei <strukov.sergei.999@gmail.com>
2023-10-12 18:03:27 +0300

    modal now filteres pokemons

--- src/components/choose-target-dialog.vue
+++ src/components/choose-target-dialog.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="choose-target-modal center">
-
     <h2>Pokémon to attack</h2>
     <ul>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">
+      <li v-for="pokemon in filteredPokemons" :key="pokemon.id">
         <button @click="selectTarget(pokemon)">{{ pokemon.name }}</button>
       </li>
     </ul>
-
   </div>
 </template>
   
@@ -17,9 +15,15 @@ export default {
     pokemons: Array, // PC's Pokémon list
     showDialog: Boolean,
   },
+  computed: {
+    filteredPokemons() {
+      // Filter the Pokémon with HP greater than zero
+      return this.pokemons.filter(pokemon => pokemon.hp > 0);
+    },
+  },
   methods: {
     selectTarget(pokemon) {
-      
+
       // Emit the selected target to the parent component
       this.$emit('target-selected', pokemon)
     },
